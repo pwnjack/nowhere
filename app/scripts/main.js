99f2110024f2f2bf1f626d87f341785aa4fff095
pwnjack <pwnjack@gmail.com>
2016-03-18 16:23:39 +0100

    finalizing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,33 @@
+// Replace all SVG images with inline SVG
+jQuery('img.svg').each(function(){
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = jQuery(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+});
+
 // FitText
 // fitText(document.getElementById('fittext'), 1.2)
 window.fitText( document.getElementsByClassName("fittext") );
@@ -44,7 +74,10 @@ Roller.Start(1); // Start the roller
 // Roller.Stop(); // Stop the roller
 
 $('body').on('click', function() {
-    
+    // Change svg color
+    $('.svg-home path').css('fill', 'white');
+    $('.svg-home:hover path').css('fill', 'white');
+    // $('.svg-home:hover').css('background', 'black');
     // Lights off
     $(this).toggleClass('is-blacked');
     // Set play cursor
@@ -57,6 +90,10 @@ $('body').on('click', function() {
         Roller.Start(1);
         // Set stop cursor
         $('body').removeClass('cursor-play').addClass('cursor-stop');
+        // Change svg color
+        $('.svg-home path').css('fill', 'black');
+        $('.svg-home:hover path').css('fill', 'white');
+        // $('.svg-home:hover').css('background', 'white');
     }
 
 });
